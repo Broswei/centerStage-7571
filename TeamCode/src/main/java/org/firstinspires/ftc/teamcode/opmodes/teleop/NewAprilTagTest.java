a755eb788e1d2f43f70bd6e42bb91f9f54f485ea
Ahmed Rahmi <arthehaxxor@gmail.com>
2023-10-26 22:13:15 -0400

    Fixed namespaced + working (?) apriltag boilerplate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/NewAprilTagTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/NewAprilTagTest.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import android.util.Size;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -17,8 +20,8 @@ public class NewAprilTagTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
-            .setDrawAxis(true)
-            .setDrawcubeProjection(true)
+            .setDrawAxes(true)
+            .setDrawCubeProjection(true)
             .setDrawTagID(true)
             .setDrawTagOutline(true)
             .build();
@@ -26,19 +29,19 @@ public class NewAprilTagTest extends LinearOpMode {
         VisionPortal visionPortal = new VisionPortal.Builder()
             .addProcessor(tagProcessor)
             .setCamera(hardwareMap.get(WebcamName.class, "webcam1")) // TODO: set webcam
-            .setCameraResoloution(new Size(640, 480)) // TODO: experiment with sizes
+            .setCameraResolution(new Size(640, 480)) // TODO: experiment with sizes, this is half resolution
             .build();
 
         waitForStart();
 
-        while(!stopIsRequested() && opModeIsActive()) {
+        while(!isStopRequested() && opModeIsActive()) {
 
             if (tagProcessor.getDetections().size() > 0) {
 
                 AprilTagDetection tag = tagProcessor.getDetections().get(0);
 
                 telemetry.addData("x", tag.ftcPose.x);
-                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("y", ((AprilTagDetection) tag).ftcPose.y);
                 telemetry.addData("z", tag.ftcPose.z);
 
                 telemetry.addData("pitch", tag.ftcPose.pitch);
